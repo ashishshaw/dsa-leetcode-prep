ddcb0cee2109541e16c8280e458905139ce8ab9f
Ashish Shaw <ashish.shaw.648@gmail.com>
2026-06-07 18:09:46 +0530

    Subsequence

---
+++ arrays-subsequence/1143_longest_common_subsequence.py
@@ -0,0 +1,16 @@
+#Bisect array and store
+
+import bisect
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        tails = []
+
+        for num in nums:
+            idx = bisect.bisect_left(tails, num)
+            if idx == len(tails):
+                tails.append(num)
+            else:
+                tails[idx] = num
+
+        return len(tails)
