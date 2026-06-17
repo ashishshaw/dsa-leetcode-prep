62f5bbfc1e1e9aa3e54ff8490405b3e5f710df25
Ashish Shaw <ashish.shaw.648@gmail.com>
2026-06-17 17:06:41 +0530

    Optimizing group anagrams

--- arrays-hashing/49_group_anagrams.py
+++ arrays-hashing/49_group_anagrams.py
@@ -1,3 +1,6 @@
+# 1st approach: O(n * k log k) -> For n words and sorting each word of length k
+# 2nd approach: O(n * k) -> Sorting avoided
+
 from collections import defaultdict
 
 class Solution:
@@ -8,3 +11,16 @@ class Solution:
             res[key].append(word)
 
         return list(res.values())
+    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for ch in s:
+                count[ord(ch) - ord('a')] += 1
+
+            groups[tuple(count)].append(s)
+
+        return list(groups.values())
