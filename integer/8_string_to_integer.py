9b652d1357327dcd9a9d5c232926aa22ae7c5e73
Ashish Shaw <ashish.shaw.648@gmail.com>
2026-07-09 23:51:36 +0530

    Str to int

---
+++ integer/8_string_to_integer.py
@@ -0,0 +1,39 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        i = 0
+        n = len(s)
+
+        # Skip leading spaces
+        while i < n and s[i] == " ":
+            i += 1
+
+        # Handle empty string after spaces
+        if i == n:
+            return 0
+
+        # Check sign
+        sign = 1
+        if s[i] == "+":
+            i += 1
+        elif s[i] == "-":
+            sign = -1
+            i += 1
+
+        # Convert digits
+        num = 0
+        while i < n and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        num *= sign
+
+        # Clamp to 32-bit signed integer range
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        if num < INT_MIN:
+            return INT_MIN
+        if num > INT_MAX:
+            return INT_MAX
+
+        return num
