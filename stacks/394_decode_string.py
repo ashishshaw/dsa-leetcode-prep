527404b8f159d8b4ab6345ecec37b0004ff9af23
Ashish Shaw <ashish.shaw.648@gmail.com>
2026-08-15 23:45:01 +0530

    approach added

--- stacks/394_decode_string.py
+++ stacks/394_decode_string.py
@@ -1,3 +1,9 @@
+#Approach: Use a stack to keep track of the current string and the number of times it should be repeated. 
+# Iterate through the input string character by character, and when encountering a digit, build the current number. 
+# When encountering an opening bracket '[', push the current string and number onto the stack and reset them. 
+# When encountering a closing bracket ']', pop from the stack and repeat the current string accordingly. 
+# Finally, return the constructed string.
+
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
